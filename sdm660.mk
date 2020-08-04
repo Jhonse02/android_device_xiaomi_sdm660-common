87db6b35fd1c21d70b4aa32eff3ba381498dbe3e
Jhonse02 <jhonse021220@gmail.com>
2020-08-04 17:43:30 -0500

    sdm660-common: Include Dirac blobs

--- sdm660.mk
+++ sdm660.mk
@@ -21,6 +21,9 @@
 # definition file).
 #
 
+# Dirac
+$(call inherit-product, vendor/dirac/dirac.mk)
+
 # Inherit properties
 $(call inherit-product, $(LOCAL_PATH)/properties.mk)
 PRODUCT_COMPATIBLE_PROPERTY_OVERRIDE := true
